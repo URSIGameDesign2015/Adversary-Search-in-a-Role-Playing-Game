aac342cc2348f67702d67f2b4ced3bd38e5c5d72
Kathryn Hodge <kahodge@vassar.edu>
2015-06-18 14:41:47 -0400

    Added turning

--- GameFramework/GameFramework/Assets/Scripts/Camera/CameraBehavior.cs
+++ GameFramework/GameFramework/Assets/Scripts/Camera/CameraBehavior.cs
@@ -5,9 +5,15 @@ public class CameraBehavior : MonoBehaviour {
 
 	public Transform target;
 	public float smoothing = 5f;
+	float camRayLength = 100f;
+	int floorMask;
 	
 	Vector3 offset;
-	
+
+//	void Awake() {
+//		floorMask = LayerMask.GetMask ("Floor");
+//	}
+
 	void Start(){
 		offset = transform.position - target.position;
 	}
@@ -16,5 +22,19 @@ public class CameraBehavior : MonoBehaviour {
 		Vector3 targetCamPos = target.position + offset;
 		transform.position = Vector3.Lerp (transform.position, targetCamPos,
 		                                   smoothing * Time.deltaTime);
+		//Turning ();
 	}
+
+//	void Turning () {
+//		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+//		RaycastHit floorHit;
+//		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+//			Vector3 playerToMouse = floorHit.point - transform.position;
+//			playerToMouse.y = 0f;
+//			
+//			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+//			transform.MoveRotation (newRotation);
+//		}
+	//}
+
 }
