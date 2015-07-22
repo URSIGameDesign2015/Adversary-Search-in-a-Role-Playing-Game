515deb1d069d0fc73f4a30f1456f5617b07b3265
kellyyu5195 <keyu@vassar.edu>
2015-07-22 11:23:58 -0400

    working on poster animation and code

--- ThirdPersonControllerTest/Assets/Scripts/PosterChoice.cs
+++ ThirdPersonControllerTest/Assets/Scripts/PosterChoice.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class PosterChoice : MonoBehaviour {
 
-	Animator anim;
-	double posterIndicator;
+//	Animator anim;
+//	double posterIndicator;
+//
+//	void Awake () {
+//		anim = GetComponent<Animator> ();
+//	}
+//
+//	void Update () {
+//		posterIndicator = Input.GetKey ();
+//		if (posterIndicator == "1") {
+//			return;
+//		}
 
-	void Awake () {
-		anim = GetComponent<Animator> ();
-	}
-
-	void Update () {
-		posterIndicator = Input.GetKey ();
-		if (posterIndicator == "1") {
-			return;
-		}
-
-	}
+//	}
 
 }
