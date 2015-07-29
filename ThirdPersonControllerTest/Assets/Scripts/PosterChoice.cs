51cdea8f7e1fb0f1ee6e772b688f841f64b1fc4d
kellyyu5195 <keyu@vassar.edu>
2015-07-29 15:14:28 -0400

    poster choice working

--- ThirdPersonControllerTest/Assets/Scripts/PosterChoice.cs
+++ ThirdPersonControllerTest/Assets/Scripts/PosterChoice.cs
@@ -3,17 +3,43 @@ using System.Collections;
 
 public class PosterChoice : MonoBehaviour {
 
+	GameObject player;
 	Animator anim;
+	PosterRendering posterRenderingScript;
+	public GameObject poster1;
+	public GameObject poster2;
+	public GameObject poster3;
 
 	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		posterRenderingScript = player.GetComponent <PosterRendering> ();
 		anim = GetComponent<Animator> ();
 	}
 
 	void Update () {
-		if (Input.GetKey (KeyCode.A)){
-			anim.SetTrigger("PosterChoice");
+		if (Input.GetKey (KeyCode.A)) {
+			anim.SetTrigger ("PosterChoice");
 		}
-
 	}
 
+	void FixedUpdate (){
+		PickPoster ();
+	}
+
+	void PickPoster() {
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			posterRenderingScript.poster = poster1;
+		} else {
+			if (Input.GetKey (KeyCode.Alpha2)) {
+				posterRenderingScript.poster = poster2;
+			} else {
+				if (Input.GetKey (KeyCode.Alpha3)) {
+					posterRenderingScript.poster = poster3;
+				} else {
+					return;
+				}
+			}
+		}
+		anim.SetTrigger ("ChoiceOff");
+	}
 }
